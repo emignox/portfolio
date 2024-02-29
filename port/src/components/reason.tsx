1d42b2a33578a3a1b8c7d9f32ed92bee450a4132
emanuele <emanuele971@icloud.com>
2024-02-29 11:14:20 +0100

    Updates

--- port/src/components/reason.tsx
+++
@@ -1,9 +0,0 @@
-function reason() {
-  return (
-    <p className="text-white  text-3xl font-bold  m-5 leading-10 opacity-80 ">
-      I'am Emanuele Polizzotto <br /> a frontend developer <br /> based in
-      brussels
-    </p>
-  );
-}
-export default reason;
