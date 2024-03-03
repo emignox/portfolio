ffc657cf029bcd01809b80d215e277f1e3bf9c75
emanuele <emanuele971@icloud.com>
2024-03-03 19:26:34 +0100

    correction for mobile

--- port/src/App.tsx
+++ port/src/App.tsx
@@ -1,12 +1,12 @@
 import Home from "./pages/home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/aboutMe";
 import Scroll from "./pages/projects";
 
 function App() {
   return (
     <>
-      <Router basename="/portfolio/">
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
