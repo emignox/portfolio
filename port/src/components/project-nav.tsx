1571c7fe755f89eed7bf4a1cc04f6e462f74813d
emanuele <emanuele971@icloud.com>
2024-05-08 11:16:51 +0200

    ciao

--- port/src/components/project-nav.tsx
+++ port/src/components/project-nav.tsx
@@ -22,28 +22,28 @@ const SidebarProjects: React.FC<SidebarProjectsProps> = ({
   return (
     <>
       <FaArrowLeft
-        className={`fixed top-1/4 right-0 z-10 text-3xl font-black cursor-pointer text-white transition-all duration-400 ${isMenuOpen ? "opacity-0" : "opacity-100"}`}
+        className={`fixed top-1/4 right-0 z-10 text-3xl font-black cursor-pointer text-white bg-gray-900 px-2 py-2   rounded-lg transition-all duration-400 ${isMenuOpen ? "opacity-0" : "opacity-100"}`}
         onMouseEnter={() => setIsMenuOpen(true)}
       />
       <div
-        className={`fixed top-1/4 right-0 z-10 h-[50vh] p-5 transition-all duration-400 transform  rounded-s-xl bg-black shadow-2xl flex items-start justify-end ${isMenuOpen ? "-translate-x-0 opacity-100 pointer-events-auto" : "translate-x-full opacity-0 pointer-events-none"}`}
+        className={`fixed top-1/4 right-0 z-10 h-[50vh] p-5 transition-all duration-400 transform  rounded-s-xl  bg-gray-900 shadow-2xl flex items-start justify-end ${isMenuOpen ? "-translate-x-0 opacity-100 pointer-events-auto" : "translate-x-full opacity-0 pointer-events-none"}`}
         onMouseLeave={() => setIsMenuOpen(false)}
       >
         <div
           className={`p-5 transition-all h-full duration-400 transform flex flex-col justify-end w-full ${isMenuOpen ? "opacity-100" : "opacity-0"}`}
         >
-          <ul className="flex flex-col items-start justify-center w-full h-2/4 gap-y-5 text-white font-bold ">
+          <ul className="flex flex-col items-start justify-center w-full font-bold text-white h-2/4 gap-y-5 ">
             {projects.map((project, index) => (
               <li
                 key={index}
-                className=" text-lg w-full flex items-center group  list-items "
+                className="flex items-center w-full text-lg group list-items"
                 onClick={() => handleMenu(project)}
               >
                 {project.title}
               </li>
             ))}
           </ul>
-          <div className=" flex w-full h-full justify-end items-end">
+          <div className="flex items-end justify-end w-full h-full ">
             <button onClick={() => setIsMenuOpen(false)} className="text-white">
               close <IoCloseSharp className="inline" />{" "}
             </button>
--- port/src/components/projects1.tsx
+++ port/src/components/projects1.tsx
@@ -13,24 +13,27 @@ function Projects() {
   return (
     <>
       <Video />
-      <div className="flex flex-col items-center">
-        <h1 className="text-2xl pb-10 text-white font-black">MY PROJECTS</h1>
+      <h1 className="pb-10 text-2xl font-black text-center text-white lg:pb-0">
+        MY PROJECTS
+      </h1>
+
+      <div className="flex flex-col items-center justify-center  lg:h-[85vh]">
         <Nav_projects onProjectClick={handleProjectClick} />
 
         {projects.map((project, index) => (
           <div key={index} onClick={() => handleProjectClick(project)}>
             {project === currentProject && (
-              <div className="flex flex-col items-center justify-center gap-y-6  lg:flex lg:flex-row lg:justify-center lg:items-center lg:mt-32 lg:px-32 ">
+              <div className="flex flex-col items-center justify-center gap-y-6 lg:flex lg:flex-row lg:justify-center lg:items-center lg:px-32 ">
                 <img
-                  className="w-full px-3 rounded-3xl  lg:w-2/5"
+                  className="w-full px-3 rounded-3xl lg:w-2/5"
                   src={project.img}
                   alt={project.title}
                 />
                 <div className="flex flex-col items-center gap-y-5">
-                  <h2 className="text-center text-white text-3xl font-black">
+                  <h2 className="text-3xl font-black text-center text-white">
                     {project.title}
                   </h2>
-                  <p className="w-3/4 text-white text-justify text-2xl ">
+                  <p className="w-3/4 text-2xl text-justify text-white ">
                     {project.description.info}
                   </p>
                   <button>
@@ -38,7 +41,7 @@ function Projects() {
                       href={project.link}
                       target="_blank"
                       rel="noreferrer"
-                      className="text-white py-2 px-2 border-2 border-white rounded-lg hover:bg-white hover:text-black transition-all duration-300"
+                      className="px-2 py-2 text-white transition-all duration-300 border-2 border-white rounded-lg hover:bg-white hover:text-black"
                     >
                       See project
                     </a>
